c56d7763accb6fa306ce734116e8cdc7a68e256b
Anastar12 <ann.star7@yandex.ru>
2024-12-13 13:22:18 +0300

    add functions create card and delete csrd

--- scripts/index.js
+++ scripts/index.js
@@ -17,5 +17,88 @@ function createCard(cardData) {
 }
 
 // @todo: Функция удаления карточки
+function handleCardDelete(event) {
+    const cardElement = event.target.closest('.places__item');
+  
+    if (cardElement) {
+      cardElement.remove();
+    }
+  }
+  
+const deleteButtons = document.querySelectorAll('.card__delete-button');
+  
+deleteButtons.forEach(button => {
+    button.addEventListener('click', handleCardDelete);
+});
+  
+function createCard(cardData) {
+    const cardTemplate = document.querySelector('#card-template').content;
+    const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
+    const cardTitle = cardElement.querySelector('.card__title');
+    const cardImage = cardElement.querySelector('.card__image');
+    const deleteButton = cardElement.querySelector('.card__delete-button');
+  
+    cardTitle.textContent = cardData.name;
+    cardImage.src = cardData.link;
+    cardImage.alt = cardData.name;
+
+    deleteButton.addEventListener('click', handleCardDelete);
+  
+    return cardElement;
+}
+  
+
 
 // @todo: Вывести карточки на страницу
+const placesList = document.querySelector('.places__list');
+
+initialCards.forEach((cardData) => {
+    const cardElement = createCard(cardData);
+    placesList.appendChild(cardElement);
+});
+
+// Модальные окна
+const profilePopup = document.querySelector('.popup_type_edit');
+const cardPopup = document.querySelector('.popup_type_new-card');
+const imagePopup = document.querySelector('.popup_type_image');
+
+const editButton = document.querySelector('.profile__edit-button');
+
+const profileFormElement = profilePopup.querySelector('.popup__form');
+const nameInput = profileFormElement.querySelector('.popup__input_type_name');
+const jobInput = profileFormElement.querySelector('.popup__input_type_description');
+
+const closeButton = document.querySelector('.popup__close');
+
+function openModal(popup) {      
+    popup.classList.add('popup_is-opened');
+}
+
+function closeModal(popup) {      
+    popup.classList.remove('popup_is-opened');
+}
+
+function handleProfileFormSubmit(evt) {
+    evt.preventDefault(); 
+
+    const nameValue = nameInput.value;
+    const jobValue = jobInput.value;
+
+    const profileTitle = document.querySelector('.profile__title');
+    const profileDescription = document.querySelector('.profile__description');
+
+    profileTitle.textContent = nameValue;
+    profileDescription.textContent = jobValue;
+
+    closeModal(profilePopup);
+}
+
+profileFormElement.addEventListener('submit', handleProfileFormSubmit);
+
+editButton.addEventListener('click', function() {
+    openModal(profilePopup); 
+}); 
+
+closeButton.addEventListener('click', function() {
+    closeModal(profilePopup); 
+}); 
